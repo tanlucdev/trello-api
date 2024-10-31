67ae88311160a4ccb9f6c68e2fd4723de06be9d3
tanlucdev <tanluc.dev@gmail.com>
2024-10-31 14:06:49 +0700

    Validate model

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,7 +1,7 @@
-import Joi from "joi"
+import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { GET_DB } from "~/config/mongodb"
-import { async } from "@babel/runtime/helpers/regeneratorRuntime"
 
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
@@ -18,18 +18,21 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
 const createNew = async (data) => {
   try {
-    const createBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    const validData = await validateBeforeCreate(data)
+    const createBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
     return createBoard
   } catch (error) { throw new Error(error) }
 }
 
 const findOneById = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: id
-    })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
     return result
   } catch (error) {
     throw new Error(error)
